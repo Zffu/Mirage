40009432560c33b180a64a2380c2413c58ca4fad
Zffu <radi0o0@outlook.com>
2025-01-30 21:57:10 +0100

    feat: changed kernel branding

--- kernel/main.c
+++ kernel/main.c
@@ -3,12 +3,5 @@
 void main() {
     clearScreen();
 
-    /* Fill up the screen */
-    int i = 0;
-    for (i = 0; i < 24; i++) {
-        printAt("Loading WalterOS...",0,i);
-    }
-
-    printAt("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
-    print("And with this text, the kernel will scroll again, and row 1 will disappear too!");
+    print("Loading Mirage v1.0.0");
 }
